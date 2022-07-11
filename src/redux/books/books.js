025e30873b7cfb741826f9df0419c1d2f2fa533b
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-07-11 13:21:39 +0100

    Test of reducer/action

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,4 +1,10 @@
-export default function reducer(state = {}, action = {}) {
+import AddBooks from '../../components/AddBooks';
+import DisplayBooks from '../../components/DisplayBooks';
+
+export default function reducer(
+  state = { AddBooks, DisplayBooks },
+  action = { type: 'newbook' },
+) {
   switch (action.type) {
     default:
       return state;
