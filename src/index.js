b38cde715b1b33213502c9d0ee3b9999c1fbf9e4
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-07-11 13:43:01 +0100

    Provider add

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import configureStore from './redux/configureStore';
 import './index.css';
 import BookStore from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <BookStore />
-    </BrowserRouter>
+    <provider store={configureStore}>
+      <BrowserRouter>
+        <BookStore />
+      </BrowserRouter>
+    </provider>
   </React.StrictMode>,
 );
